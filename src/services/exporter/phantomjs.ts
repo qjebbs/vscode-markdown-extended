ab34ec22c00433d0f22b6720904936fc214bda97
jebbs <qjebbs@gmail.com>
2018-04-13 11:28:14 +0800

    phantomExport style override

--- src/services/exporter/phantomjs.ts
+++ src/services/exporter/phantomjs.ts
@@ -10,7 +10,27 @@ export function phantomExport(
     fileName: string,
     callback: (err: Error, res: pdf.FileInfo) => void
 ) {
+    let inject = "";
+    switch (document.meta.phantomConfig.type) {
+        case "pdf":
+            inject = `/* injected by phantomExport */
+                    body, .vscode-body {
+                        max-width: 100% !important;
+                        width: 1000px !important;
+                        margin: 0 !important;
+                        padding: 0 !important;
+                    }`
+            break;
+        case "png":
+        case "jpeg":
+            inject = `/* injected by phantomExport */
+                    body, .vscode-body {
+                        width: 1000px !important;
+                    }`
+        default:
+            break;
+    }
     mkdirsSync(path.dirname(fileName));
-    let html = renderHTML(document, true);
+    let html = renderHTML(document, true, inject);
     pdf.create(html, document.meta.phantomConfig).toFile(fileName, callback);
 }
